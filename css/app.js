2629585c7ea2114797400c2a567303d6eded8851
salolevi <salolevi2@gmail.com>
2022-06-09 18:41:38 -0300

    responsiveness full HD aora si

--- css/app.js
+++ css/app.js
@@ -1,7 +1,7 @@
 const container = document.querySelector('.container');
 
 window.addEventListener('resize', ()=> {
-  if (window.innerWidth < 1143) {
+  if (window.innerWidth <= 1099) {
     document.querySelector('.delivery').style.height = `${document.querySelector('.podio').clientHeight}px`
   } else {
     document.querySelector('.delivery').style.height = 'initial';
