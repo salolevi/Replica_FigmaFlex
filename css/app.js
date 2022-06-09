1129298e7b0dc53f6a280f657253de6bd78440eb
salolevi <salolevi2@gmail.com>
2022-06-09 18:20:58 -0300

    responsiveness full HD bug fixed

--- css/app.js
+++ css/app.js
@@ -3,5 +3,7 @@ const container = document.querySelector('.container');
 window.addEventListener('resize', ()=> {
   if (window.innerWidth < 1143) {
     document.querySelector('.delivery').style.height = `${document.querySelector('.podio').clientHeight}px`
+  } else {
+    document.querySelector('.delivery').style.height = 'initial';
   }
 })
