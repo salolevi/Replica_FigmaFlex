3eaf7a361987ded9b8bbc11a6288d85646119588
salolevi <salolevi2@gmail.com>
2022-06-09 18:18:37 -0300

    responsiveness full HD

---
+++ css/app.js
@@ -0,0 +1,7 @@
+const container = document.querySelector('.container');
+
+window.addEventListener('resize', ()=> {
+  if (window.innerWidth < 1143) {
+    document.querySelector('.delivery').style.height = `${document.querySelector('.podio').clientHeight}px`
+  }
+})
